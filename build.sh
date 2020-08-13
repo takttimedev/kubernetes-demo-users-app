b03ec900791c6f1ef4e45cab1344b5fb4bc20df1
ab96369 <55463930+ab96369@users.noreply.github.com>
2020-08-14 00:05:17 +0530

    buld

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 mvn clean install
-docker build -t geekhitesh/users-app .
-docker push geekhitesh/users-app
+docker build -t 041266/users-app .
+docker push 041266/users-app
 kubectl apply -f .
 #kubectl expose deployment users-app --name=users-app --dry-run -o yaml > service.yaml
+
+#https://docs.aws.amazon.com/AmazonECS/latest/developerguide/docker-basics.html
